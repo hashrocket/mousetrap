38b0eb60510dddaf88ff14cdb6878d70fa46b83f
Jon Larkowski and Sandro Turriate <dev+l4rk+sandro@hashrocket.com>
2009-08-24 22:48:26 -0400

    Add create-customer

--- lib/mousetrap/client.rb
+++ lib/mousetrap/client.rb
@@ -14,9 +14,18 @@ class Mousetrap::Client
     get 'customers'
   end
 
+  def create_customer(hash)
+    post 'customers', 'new', hash
+  end
+
+
   protected
 
   def get(resource)
     self.class.get "/xml/#{resource}/get/productCode/#{product_code}"
+  end
+
+  def post(resource, action, hash)
+    self.class.post "/xml/#{resource}/#{action}/productCode/#{product_code}", :body => hash
   end
 end
--- spec/mousetrap/client_spec.rb
+++ spec/mousetrap/client_spec.rb
@@ -1,6 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Mousetrap::Client do
+  before do
+    Mousetrap.stub :get
+    Mousetrap.stub :post
+    @client = Mousetrap::Client.new 'lark@example.com', 'abc123', 'my_product_code'
+  end
+
   describe "class" do
     it "sets the base URI" do
       Mousetrap::Client.default_options[:base_uri].should == 'https://cheddargetter.com'
@@ -19,17 +25,36 @@ describe Mousetrap::Client do
     end
 
     it "sets basic auth based on the supplied username and password" do
-      subject.should_receive(:basic_auth).with('jon@example.com', 'abc123')
-      subject.new 'jon@example.com', 'abc123', nil
+      subject.should_receive(:basic_auth).with('lark@example.com', 'abc123')
+      subject.new 'lark@example.com', 'abc123', nil
     end
   end
 
   describe "#customers" do
-    subject { Mousetrap::Client.allocate }
-
     it "gets customers" do
-      subject.should_receive(:get).with('customers')
-      subject.customers
+      @client.should_receive(:get).with('customers')
+      @client.customers
+    end
+  end
+
+  describe "#create_customer" do
+    it "creates a customer" do
+      @client.should_receive(:post).with('customers', 'new', 'some_hash')
+      @client.create_customer('some_hash')
+    end
+  end
+
+  describe "#get" do
+    it "gets /xml/<resource>/get/productCode/<my_product_code>" do
+      @client.class.should_receive(:get).with('/xml/some_resource/get/productCode/my_product_code')
+      @client.send(:get, 'some_resource')
+    end
+  end
+
+  describe "#post" do
+    it "posts to /xml/<resource>/<action>/productCode/<product_code>" do
+      @client.class.should_receive(:post).with('/xml/some_resource/some_action/productCode/my_product_code', :body => 'some_hash')
+      @client.send(:post, 'some_resource', 'some_action', 'some_hash')
     end
   end
 end
