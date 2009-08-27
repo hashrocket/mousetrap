7008b917fb60e001abcf59b3f20f2fa1d5ea3e77
Jon Larkowski <jonlarkowski@gmail.com>
2009-08-27 10:52:15 -0400

    Reorganize

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,9 @@
 Factory.define :new_customer, :class => Mousetrap::Customer, :default_strategy => :stub do |f|
+  f.add_attribute :id, nil
+  f.code { |me| me.email }
+  f.email { random_email_address }
   f.first_name 'Jon'
   f.last_name 'Larkowski'
-  f.email { random_email_address }
-  f.code { |me| me.email }
-  f.add_attribute :id, nil
 end
 
 Factory.define :existing_customer, :parent => :new_customer, :default_strategy => :stub do |f|
