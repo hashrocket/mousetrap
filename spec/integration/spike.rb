00fbd6e701dc8de95ec64436645801a664c754af
Jon Larkowski <jonlarkowski@gmail.com>
2009-09-01 17:22:03 -0400

    Hack

--- spec/integration/spike.rb
+++ spec/integration/spike.rb
@@ -1,3 +1,5 @@
+# This script is for hacking and playing around.
+
 require File.dirname(__FILE__) + '/../../lib/mousetrap'
 require 'activesupport'
 require 'factory_girl'
@@ -10,6 +12,7 @@ settings = YAML.load_file(File.dirname(__FILE__) + '/settings.yml')
 Mousetrap.authenticate(settings['user'], settings['password'])
 Mousetrap.product_code = settings['product_code']
 
+
 def plans
   all_plans = Mousetrap::Plan.all
   puts all_plans.to_yaml
@@ -18,76 +21,21 @@ def plans
   puts test_plan.to_yaml
 end
 
-
 def customers
-  all_customers = Mousetrap::Customer.all
-  puts all_customers.to_yaml
-  all_customers.each { |c| c.destroy }
-
   customer = Factory :new_customer
   customer.save
 
   api_customer = Mousetrap::Customer[customer.code]
   puts api_customer.to_yaml
 
-  all_customers = Mousetrap::Customer.all
-  puts all_customers.to_yaml
+  #all_customers = Mousetrap::Customer.all
+  #puts all_customers.to_yaml
 end
 
-customers
+def destroy_all_customers
+  all_customers = Mousetrap::Customer.all
+  all_customers.each { |c| c.destroy }
+end
 
-
-
-
-
-__END__
-
-
-
-all_customers = Mousetrap::Customer.all
-puts all_customers.inspect
-puts all_customers.to_yaml
-
-
-code = 'maasdxgliu@example.com'
-c = Mousetrap::Customer[code]
-puts c.to_yaml
-c.destroy
-
-puts '-' * 80
-c = Mousetrap::Customer[code]
-puts c.to_yaml
-
-
-
-
-
-email = random_email_address
-attributes = {
-  'code' => email,
-  'firstName' => 'Example',
-  'lastName' => 'Customer',
-  'email' => email,
-  'subscription' => {
-      'planCode' => 'TEST',
-      'ccFirstName' => 'Jon',
-      'ccLastName' => 'Larkowski',
-      'ccNumber' => '4111111111111111',
-      'ccExpiration' => '12-2012',
-      'ccZip' => '90210'
-  }
-}
-
-customer = Mousetrap::Customer.create attributes
-puts customer
-
-
-
-
-customers_hash = Mousetrap::Customer['maasdxgliu@example.com']
-customer_hash = customers_hash['customers']['customer'].slice 'firstName', 'lastName', 'email', 'code'
-customer = Mousetrap::Customer.new customer_hash
-
-customer.first_name = random_string
-puts customer.save!
-
+#destroy_all_customers
+#customers
